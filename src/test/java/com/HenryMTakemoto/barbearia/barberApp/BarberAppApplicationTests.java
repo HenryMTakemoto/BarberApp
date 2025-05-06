1785d327ea376916d49c381dfc9eaee47ef84f14
HenryMTakemoto <henry.takemoto@gmail.com>
2025-05-06 08:43:58 -0300

    Inicial commit

---
+++ src/test/java/com/HenryMTakemoto/barbearia/barberApp/BarberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HenryMTakemoto.barbearia.barberApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
