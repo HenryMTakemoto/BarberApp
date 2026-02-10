faf082167851ea022cbab7994022b23d48f2ed40
HenryMTakemoto <henry.takemoto@gmail.com>
2026-02-10 03:59:07 -0300

    feature: implementação do CRUD de especialidades

--- backend/src/main/java/com/barberapp/backend/repository/SpecialtyRepository.java
+++ backend/src/main/java/com/barberapp/backend/repository/SpecialtyRepository.java
@@ -3,7 +3,6 @@ package com.barberapp.backend.repository;
 import com.barberapp.backend.model.Specialty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
